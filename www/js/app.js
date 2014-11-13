a1febf9385c67f8b971b366ab70375ff6c3de206
Jeremy Wilken <gnomation@gnomeontherun.com>
2014-11-12 22:33:41 -0600

    add home state

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,17 @@
 angular.module('App', ['ionic'])
 
+.config(function ($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'views/home/home.html'
+    });
+  
+  $urlRouterProvider.otherwise('/home');
+
+})
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
