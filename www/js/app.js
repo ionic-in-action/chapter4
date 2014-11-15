f769e94b8e404e044a36d6a5b4779036891c01be
Jeremy Wilken <gnomation@gnomeontherun.com>
2014-11-15 15:20:56 -0600

    adding the weather view

--- www/js/app.js
+++ www/js/app.js
@@ -11,6 +11,11 @@ angular.module('App', ['ionic'])
       url: '/reservation',
       controller: 'ReservationController',
       templateUrl: 'views/reservation/reservation.html'
+    })
+    .state('weather', {
+      url: '/weather',
+      controller: 'WeatherController',
+      templateUrl: 'views/weather/weather.html'
     });
 
   $urlRouterProvider.otherwise('/home');
